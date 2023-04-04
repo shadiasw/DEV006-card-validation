64b2eaf25aa4d0cf240855fc0da8c83cc0960d53
= <=>
2023-04-04 10:10:06 -0500

    DEV006-CARD-VALIDATION agregado en la pagina

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import validator from './validator.js';
 
-console.log(validator);
+const inputTarjeta = document.getElementById("inputTarjeta");
+//var inputFechaVencimiento = document.getElementById ("selectMes , selectAño");
+//var inputCVV = document.getElementById ("inputCCV");
+const buttonClick = document.getElementById("buttonClick");
+
+console.log(inputTarjeta);
+console.log(buttonClick);
+
+buttonClick.onclick = function (e) {
+  // para que no se recargue la pag pero que si se envien datos
+  e.preventDefault();
+  const valido = validator.isValid(inputTarjeta.value);
+  const masket = validator.maskify(inputTarjeta.value);
+  console.log(masket);
+  inputTarjeta.value = "Valor ingresado desde código"
+
+  if (valido === true) {
+    alert("Numero de tarjeta " + masket + " valida, Compra exitosa");
+  } else {
+    alert("Numero de tarjeta  " + masket + " invalida, Vuelva a intentarlo");
+  }
+
+
+
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,70 @@
+function isValid(inputTarjeta) {
+  const tarjeta = inputTarjeta;
+  let contador = 0;
+  let suma = 0;
+  let duplicar = true;
+
+  //menor longuitud
+  //chartAt posiciones
+  // pars cadena a entero
+  //toString numero a cadena 
+  while (contador < tarjeta.length) {
+
+    let numero = parseInt(tarjeta.charAt(contador));
+    if (duplicar === true) {
+      numero = numero * 2;
+
+      if (numero > 9) {
+        numero = numero.toString();
+        numero = parseInt(numero.charAt(0)) + parseInt(numero.charAt(1));
+      }
+
+      duplicar = false;
+    } else {
+      duplicar = true;
+    }
+    suma = suma + numero;
+    contador = contador + 1;
+  }
+
+  if ((suma % 10) === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+
+function maskify(numeroTarjeta) {
+  //10
+  //Sarkheilaa
+  //######ilaa
+
+  const posicion = numeroTarjeta.length - 4;
+  const longitud = numeroTarjeta.length;
+
+  const porción = numeroTarjeta.substring(posicion, longitud);
+  //ilaa
+
+  //######
+  let numeroTarjetaMasked = "";
+  const cantidadNoMostrar = numeroTarjeta.length - 4;
+  let contador = 0;
+  while (contador < cantidadNoMostrar) {
+    numeroTarjetaMasked = numeroTarjetaMasked + "#";
+    contador = contador + 1;
+  }
+
+  const resultado = numeroTarjetaMasked + porción;
+  return resultado;
+
+}
+
 const validator = {
-  // ...
-};
+  isValid: isValid,
+  maskify: maskify
+}
+
 
 export default validator;
